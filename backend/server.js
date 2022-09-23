3f90346eca7a33919b1dfa758c342a86cdedae39
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2022-09-23 09:55:28 -0400

    Prepare Deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -17,6 +18,19 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", goalRoutes);
 app.use("/api/users", userRoutes);
+
+//Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
